df966a40b80db4ee4d3f3f3a9839ff3cbe62d850
Louis Nguyen <ndmn004@gmail.com>
2024-01-16 21:07:48 +0700

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Louis on 16/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
